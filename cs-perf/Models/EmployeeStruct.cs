3be037a682c67ccd37d29dd62efae6bbd1b84c94
Rajesh Pillai <pillai.rajesh@gmail.com>
2018-06-23 22:51:02 +0530

    Added struct example

--- cs-perf/Models/EmployeeStruct.cs
+++ cs-perf/Models/EmployeeStruct.cs
@@ -5,23 +5,19 @@ using System.Text;
 namespace CS_Perf.Models
 {
     [Serializable]
-    class Employee
+    struct EmployeeStruct
     {
         public int Id { get; set; }
         public string FirstName { get; set; }
 
         public List<string> Fields { get; set; }
 
-        public Employee()
-        {
 
-        }
-
-        public Employee(int id, string firstName, string[] fields)
+        public EmployeeStruct(int id, string firstName, string[] fields)
         {
             this.Id = id;
             this.FirstName = firstName;
             this.Fields = new List<string>(fields);
         }
-    }   
+    }
 }
--- cs-perf/Program.cs
+++ cs-perf/Program.cs
@@ -1,31 +1,108 @@
 ﻿using System;
 using System.Collections.Generic;
+using System.Dynamic;
 using System.IO;
 using System.Reflection;
+using System.Text;
 using CS_Perf.Models;
 
 namespace CS_Perf
 {
     class Program
     {
+        const int size = 10000000;
+        const string FILE_NAME = "1-lac-3-fields.csv";
+
         static void Main(string[] args)
         {
-            CreateCsvFile(5000000);  // 50 lac
-            TestArraySize(10000000);
+            //CreateCsvFile(size,50); 
+            //TestArraySize(size);
 
-            List<Employee> employees = ReadCsvFile();
+            TestListOfEmployes();
+            TestDictOfEmployes();
+            TestListOfEmployeesStruct();
+          
 
-            Console.WriteLine(String.Format("Read {0} employees.", employees.Count));
-
+            /*
             var sizeInBytes = MemSize<List<Employee>>.SizeOf(employees);
 
             Console.WriteLine("Size List<Employee>[100000] :{0} in bytes", sizeInBytes);
             Console.WriteLine("Size List<Employee>[100000] :{0} in MB", ConvertBytesToMegabytes(sizeInBytes));
-
-
+            */
 
             Console.ReadLine();
         }
+
+        static void TestListOfEmployeesStruct ()
+        {
+            long before = GC.GetTotalMemory(true);
+            List<EmployeeStruct> employees = ReadCsvFileAsStruct();
+            long after = GC.GetTotalMemory(true);
+
+            Console.WriteLine(String.Format("Read {0} employees.", employees.Count));
+
+
+            var diff = after - before;
+
+            Console.WriteLine("Total Memory (Bytes): List<EmployeesStruct> {0}", diff);
+            Console.WriteLine("Total Memory (MB): List<EmployeesStruct> {0}", ConvertBytesToMegabytes(diff));
+            Console.WriteLine("Total Memory (GB): List<EmployeesStruct> {0}", ConvertBytesToMegabytes(diff) / 1024);
+
+            Console.WriteLine("Per Employee: " + diff / size);
+
+            // Stop the GC from messing up our measurements
+            GC.KeepAlive(employees);
+
+        }
+
+        static void TestListOfEmployes()
+        {
+
+            long before = GC.GetTotalMemory(true);
+            List<Employee> employees = ReadCsvFileAsList();
+            long after = GC.GetTotalMemory(true);
+
+            Console.WriteLine(String.Format("Read {0} employees.", employees.Count));
+
+
+            var diff = after - before;
+
+            Console.WriteLine("Total Memory (Bytes): List<Employees> {0}", diff);
+            Console.WriteLine("Total Memory (MB): List<Employees> {0}", ConvertBytesToMegabytes(diff));
+            Console.WriteLine("Total Memory (GB): List<Employees> {0}", ConvertBytesToMegabytes(diff)/1024);
+
+            Console.WriteLine("Per Employee: " + diff / size);
+
+            // Stop the GC from messing up our measurements
+            GC.KeepAlive(employees);
+
+        }
+
+
+        static void TestDictOfEmployes()
+        {
+
+            long before = GC.GetTotalMemory(true);
+            List<dynamic> employees = ReadCsvFileAsDict();
+            long after = GC.GetTotalMemory(true);
+
+            Console.WriteLine(String.Format("DICT: Read {0} employees.", employees.Count));
+
+
+            var diff = after - before;
+
+            Console.WriteLine("Total Memory (Bytes): DICT<> {0}", diff);
+            Console.WriteLine("Total Memory (MB): DICT<> {0}", ConvertBytesToMegabytes(diff));
+            Console.WriteLine("Total Memory (GB): DICT<> {0}", ConvertBytesToMegabytes(diff) / 1024);
+
+            Console.WriteLine("Per Employee: " + diff / size);
+
+            // Stop the GC from messing up our measurements
+            GC.KeepAlive(employees);
+
+        }
+
+
 
         static void TestArraySize(int size)
         {
@@ -41,15 +118,19 @@ namespace CS_Perf
 
             double diff = after - before;
 
-            Console.WriteLine("Per object: " + diff / size);
+            Console.WriteLine("Array Per object: " + diff / size);
 
             // Stop the GC from messing up our measurements
             GC.KeepAlive(array);
         }
 
-        static List<Employee> ReadCsvFile()
+        static string GetCSVFileName()
         {
-            string filePath = Path.GetDirectoryName(Assembly.GetEntryAssembly().Location) + @"\data\employees.csv";
+            return Path.GetDirectoryName(Assembly.GetEntryAssembly().Location) + @"\data\" + FILE_NAME;
+        }
+        static List<Employee> ReadCsvFileAsList()
+        {
+            string filePath = GetCSVFileName();
             List<Employee> employees = new List<Employee>();
             char[] separators = { ',' };
             string[] input;
@@ -63,7 +144,7 @@ namespace CS_Perf
                 input = data.Split(separators, StringSplitOptions.RemoveEmptyEntries);
                 var id = Convert.ToInt32(input[0]);
                 var firstName = input[1];
-                Employee e = new Employee(id, firstName);
+                Employee e = new Employee(id, firstName, input);
 
                 employees.Add(e);
                 //Console.WriteLine(input[1]);
@@ -72,20 +153,96 @@ namespace CS_Perf
             return employees;
         }
 
-        static void CreateCsvFile(int noOfRecords)
+        static List<EmployeeStruct> ReadCsvFileAsStruct()
+        {
+            string filePath = GetCSVFileName();
+            List<EmployeeStruct> employees = new List<EmployeeStruct>();
+            char[] separators = { ',' };
+            string[] input;
+
+            StreamReader sr = new StreamReader(filePath);
+
+            string data = string.Empty;
+
+            while ((data = sr.ReadLine()) != null)
+            {
+                input = data.Split(separators, StringSplitOptions.RemoveEmptyEntries);
+                var id = Convert.ToInt32(input[0]);
+                var firstName = input[1];
+                EmployeeStruct e = new EmployeeStruct(id, firstName, input);
+
+                employees.Add(e);
+                //Console.WriteLine(input[1]);
+            }
+
+            return employees;
+        }
+
+
+        static List<dynamic> ReadCsvFileAsDict()
+         {
+            string filePath = GetCSVFileName();
+
+            List<dynamic> employees = new List<dynamic>();
+
+            char[] separators = { ',' };
+            string[] input;
+
+            StreamReader sr = new StreamReader(filePath);
+
+            string data = string.Empty;
+
+            
+            while ((data = sr.ReadLine()) != null)
+            {
+                int i = 0;
+
+                IDictionary<string, object> fields = new ExpandoObject();
+
+                input = data.Split(separators, StringSplitOptions.RemoveEmptyEntries);
+                var id = Convert.ToInt32(input[0]);
+                var firstName = input[1];
+
+
+                foreach(var f in input)
+                {
+                    fields["field" + i.ToString()] = f;
+                    i++;
+                }
+
+                employees.Add(fields);
+                //Console.WriteLine(input[1]);
+            }
+
+            return employees;
+        }
+
+        static void CreateCsvFile(int noOfRecords, int noOfFields)
         {
             string filePath = Path.GetDirectoryName(Assembly.GetEntryAssembly().Location) +  @"\data\employees.csv";
 
             Console.WriteLine(filePath);
 
+            List<string> fields = new List<string>();
+            string field = "id";
             using (var w = new StreamWriter(filePath))
             {
                 for ( var i = 1; i <= noOfRecords; i++)
                 {
                     var id = i.ToString();
-                    var name = "name " + i.ToString();
-                    var line = string.Format("{0},{1}", id, name);
+                    fields.Add(id);
+
+                    for(var col = 0; col < noOfFields; col++)
+                    {
+                        field = string.Format("field-{0}-{1}", i, col); // "field-" + i.ToString() + col.ToString();
+                        fields.Add(field);
+                    }
+                    var line = string.Join(",", fields);
+
                     w.WriteLine(line);
+
+                    fields = new List<string>();
+
                     w.Flush();
                 }
             }
